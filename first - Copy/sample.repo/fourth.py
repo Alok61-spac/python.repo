fe994e68b3cc7fa755c5f6176bcd0e4d969c8209
alok-666 <alokmalik8261@gmail.com>
2026-04-12 09:54:32 +0530

    Add files via upload

---
+++ first - Copy/sample.repo/fourth.py
@@ -0,0 +1,10 @@
+A = (82,93,0,93,8,61,199,83,81,84)
+print(type(A))
+print(len(A))
+x = 61
+i =0 
+while i < len(A):
+    if(A[i] == x):
+
+        print("the number occurs at index :",i)
+    i += 1
---
+++ first - Copy/sample.repo/one.py
@@ -0,0 +1,5 @@
+print("hi")
+a=int(input("variable1 :"))
+b=int(input("variable2 :"))
+print("avg =", (a + b) / 2)
+
