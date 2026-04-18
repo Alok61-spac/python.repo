12ef0f19677109804178d1b61cf7e813fbe5f748
alok-666 <alokmalik8261@gmail.com>
2026-04-18 13:17:54 +0530

    Add files via upload
    18.0426

---
+++ loops.py
@@ -0,0 +1,43 @@
+for i in range(1,20,1):
+    print(i * " * ")
+
+
+
+
+
+
+for i in range(20,-1,-1):
+    print( " * " * i)
+
+
+
+
+
+for i in range(1,20,1):
+    for j in range(1,i + 1):
+        print(j , end = "")
+    print()
+
+
+
+
+
+for i  in range(20,-1,-1):
+    for j in range(1,i + 1):
+        print(j , end = "")
+    print()
+
+
+
+
+    
+
+found = False
+A = [82,80,85,92,91,99,199,87,54,61,68,28]
+X = int(input("enter your value : "))
+for value in A:
+    if value == X :
+        print("find it..",)
+        found = True
+if not found:
+    print("Enter A Valid Value ")
